c2219e1e7c7d9d62abb3a85f174c56ceb493ea79
Giovanni Lovato <heruan@aldu.net>
2017-04-20 15:08:27 +0200

    chore(package): update dependencies

--- dist/main/index.js
+++ dist/main/index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var progress_indicator_1 = require("./progress-indicator");
 exports.ProgressIndicator = progress_indicator_1.ProgressIndicator;
 
